dc2d7fd9a692e343eb29511f8f0fd1d5cf0da718
knastnt <456kot@mail.ru>
2018-04-18 09:30:50 +1000

    конвертация в системы счисления

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2311_TreeMap/test.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2311_TreeMap/test.java
@@ -12,6 +12,9 @@ public class test {
         tst.put("5", "Шесть");
         tst.put("seven", "8");
         tst.put("Девять", "ten");
+        tst.put(String.valueOf(Integer.MIN_VALUE), String.valueOf(Byte.MIN_VALUE));
+        tst.put("123-to-binary", String.valueOf(Integer.toBinaryString(123)));
+        tst.put("123-to-hex", String.valueOf(Integer.toHexString(123)));
 
         for(Map.Entry<String, String> m : tst.entrySet()){
             System.out.println(m.getKey() + " = " + m.getValue());
