d4d7a9afd198ef6e75949385c6d9665d72dd33fb
knastnt <456kot@mail.ru>
2018-04-28 11:33:08 +1000

    2405

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2405/Solution.java
@@ -0,0 +1,105 @@
+package com.javarush.task.task24.task2405;
+
+/* 
+Black box
+*/
+public class Solution implements Action {
+    public static int countActionObjects;
+
+    private int param;
+
+    private Action solutionAction = new Action() {
+        //!!!!! Changes can be here
+        //!!!!! Изменения могут быть тут
+
+        public void someAction() {
+            //!!!!! All changes have to be here
+            //!!!!! Все изменения должны быть только тут
+            /*if(param>0){
+                new FirstClass(){
+                    @Override
+                    public Action getDependantAction() {
+                        return null;
+                    }
+
+                    @Override
+                    public void someAction() {
+                        if(param>0){
+                            System.out.println(param--);
+                            this.someAction();
+                        }else {
+                            //super.someAction();
+                            super.someAction();
+                            solutionAction.someAction();
+                        }
+                    }
+                }.someAction();
+            }else{
+                SecondClass sc = new SecondClass(){
+                    StringBuilder sb = new StringBuilder(SPECIFIC_ACTION_FOR_ANONYMOUS_SECOND_CLASS_PARAM).append(param).deleteCharAt(0);
+
+
+                    public void someAction() {
+                        super.someAction();
+                        System.out.println(sb.toString());
+                    }
+
+                };
+                sc.someAction();
+
+            }*/
+            if (param > 0){
+                while (param > 0) {
+                    System.out.println(param--);
+                }
+                new FirstClass() {
+                    @Override
+                    public Action getDependantAction() {
+                        super.someAction();
+                        return null;
+                    }
+                }.someAction();
+            }
+            new SecondClass(){
+                @Override
+                public void someAction() {
+                    System.out.println(sb.append(SecondClass.SPECIFIC_ACTION_FOR_ANONYMOUS_SECOND_CLASS_PARAM).append(param).toString());
+                }
+            }.someAction();
+        }
+    };
+
+
+    public Solution(int param) {
+        this.param = param;
+    }
+
+    @Override
+    public void someAction() {
+        solutionAction.someAction();
+    }
+
+    /**
+     * 5
+     * 4
+     * 3
+     * 2
+     * 1
+     * class FirstClass, method someAction
+     * class SecondClass, method someAction
+     * Specific action for anonymous SecondClass, param = 0
+     * Count of created Action objects is 2
+     * class SecondClass, method someAction
+     * Specific action for anonymous SecondClass, param = -1
+     * Count of created Action objects is 3
+     */
+    public static void main(String[] args) {
+        Solution solution = new Solution(5);
+        solution.someAction();
+        System.out.println("Count of created Action objects is " + countActionObjects);
+
+        solution = new Solution(-1);
+        solution.someAction();
+        System.out.println("Count of created Action objects is " + countActionObjects);
+    }
+}
