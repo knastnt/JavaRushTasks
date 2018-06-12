04aef730ff5e60b86d5d955b8ec71bce1ac31e2d
knastnt <456kot@mail.ru>
2018-06-13 08:49:32 +1000

    2510

--- 3.JavaMultithreading/src/com/javarush/task/task25/task2509/SocketTask.java
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2509/SocketTask.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task25.task2509;
 
+import java.io.IOException;
 import java.net.Socket;
 import java.util.concurrent.FutureTask;
 import java.util.concurrent.RunnableFuture;
@@ -13,6 +14,11 @@ public abstract class SocketTask<T> implements CancellableTask<T> {
 
     public synchronized void cancel() {
         //close all resources here
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public RunnableFuture<T> newTask() {
@@ -20,6 +26,14 @@ public abstract class SocketTask<T> implements CancellableTask<T> {
             public boolean cancel(boolean mayInterruptIfRunning) {
                 //close all resources here by using proper SocketTask method
                 //call super-class method in finally block
+                try {
+                    socket.close();
+                }catch (Exception e){
+
+                }finally {
+                    super.cancel(true);
+                }
+
                 return false;
             }
         };
