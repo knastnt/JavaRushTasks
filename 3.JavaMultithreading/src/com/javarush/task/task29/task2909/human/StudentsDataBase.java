d4ac5c08ad1dc8167be0cfab26d6875f4879b523
knastnt <456kot@mail.ru>
2018-08-12 00:08:38 +1000

    рефакторинг8

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/StudentsDataBase.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/StudentsDataBase.java
@@ -6,35 +6,49 @@ import java.util.List;
 public class StudentsDataBase {
     public static List<Student> students = new ArrayList<>();
 
-    public static void addInfoAboutStudent(String name, int age, double averageGrade) {
+    /*public static void addInfoAboutStudent(String name, int age, double averageGrade) {
         Student student = new Student(name, age, averageGrade);
         students.add(student);
         printInfoAboutStudent(student.getName(), student);
 
+    }*/
+
+    public static void addInfoAboutStudent(Student student) {
+        students.add(student);
+        printInfoAboutStudent(student);
+
     }
 
-    public static void printInfoAboutStudent(String name, Student student) {
+    /*public static void printInfoAboutStudent(String name, Student student) {
         System.out.println("Имя: " + name + " Возраст: " + student.getAge());
+    }*/
+
+    public static void printInfoAboutStudent(Student student) {
+        System.out.println("Имя: " + student.getName() + " Возраст: " + student.getAge());
     }
 
-    public static void removeStudent(int index) throws IndexOutOfBoundsException {
-        students.remove(index);
+    public static void removeStudent(int index) {
+        if(students != null && students.size()-1 >= index && index >= 0) {
+            students.remove(index);
+        }
     }
 
     public static void findDimaOrSasha() {
-        boolean found = false;
+        //boolean found = false;
         for (int i = 0; i < students.size(); i++) {
-            if (!found) {
+            //if (!found) {
                 if (students.get(i).getName().equals("Dima")) {
                     System.out.println("Студент Dima есть в базе.");
-                    found = true;
+                    //found = true;
+                    break;
                 }
 
                 if (students.get(i).getName().equals("Sasha")) {
                     System.out.println("Студент Sasha есть в базе.");
-                    found = true;
+                    //found = true;
+                    break;
                 }
-            }
+            //}
         }
     }
 }
