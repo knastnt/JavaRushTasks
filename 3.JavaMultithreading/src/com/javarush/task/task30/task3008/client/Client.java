c3bf7a59ca42f315b2b2623e3ef0f2622ad2b7f6
knastnt <456kot@mail.ru>
2018-10-31 09:29:17 +1000

    чат 13

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -0,0 +1,45 @@
+package com.javarush.task.task30.task3008.client;
+
+import com.javarush.task.task30.task3008.Connection;
+import com.javarush.task.task30.task3008.ConsoleHelper;
+import com.javarush.task.task30.task3008.Message;
+import com.javarush.task.task30.task3008.MessageType;
+
+import java.io.IOException;
+
+public class Client {
+    protected Connection connection;
+    private volatile boolean clientConnected = false;
+
+    public class SocketThread extends Thread{
+
+    }
+
+    protected String getServerAddress(){
+        ConsoleHelper.writeMessage("Введите IP адрес сервера:");
+        return ConsoleHelper.readString();
+    }
+    protected int getServerPort(){
+        ConsoleHelper.writeMessage("Введите порт сервера:");
+        return ConsoleHelper.readInt();
+    }
+    protected String getUserName(){
+        ConsoleHelper.writeMessage("Введите Ваше имя:");
+        return ConsoleHelper.readString();
+    }
+    protected boolean shouldSendTextFromConsole(){
+        return true;
+    }
+    protected SocketThread getSocketThread(){
+        return new SocketThread();
+    }
+    protected void sendTextMessage(String text){
+        try{
+            Message message = new Message(MessageType.TEXT, text);
+            connection.send(message);
+        }catch (IOException e){
+            ConsoleHelper.writeMessage("Error in sendTextMessage");
+            clientConnected = false;
+        }
+    }
+}
