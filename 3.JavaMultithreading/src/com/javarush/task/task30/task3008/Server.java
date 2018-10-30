26c6c4de33e6bfd8d9e20149eef75eaa297ba71d
knastnt <456kot@mail.ru>
2018-10-30 16:53:44 +1000

    чат 7

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -3,8 +3,12 @@ package com.javarush.task.task30.task3008;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
     public static void main(String[] args) {
         ConsoleHelper.writeMessage("Введите порт сервера:");
         int port = ConsoleHelper.readInt();
@@ -37,6 +41,17 @@ public class Server {
 
         public Handler(Socket socket) {
             this.socket = socket;
+        }
+    }
+
+    public static void sendBroadcastMessage(Message message){
+        for(Map.Entry<String, Connection> entry : connectionMap.entrySet()){
+            try {
+                entry.getValue().send(message);
+            } catch (IOException e) {
+                //e.printStackTrace();
+                ConsoleHelper.writeMessage("не удалось доставить сообщение пользователю " + entry.getKey());
+            }
         }
     }
 }
