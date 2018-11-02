77429909d8c57a36d7a56d6d65e677f326bdec29
knastnt <456kot@mail.ru>
2018-11-02 19:03:40 +1000

    2707

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2707/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2707/Solution.java
@@ -1,6 +1,9 @@
 package com.javarush.task.task27.task2707;
 
-/* 
+import static java.lang.Thread.enumerate;
+import static java.lang.Thread.sleep;
+
+/*
 Определяем порядок захвата монитора
 */
 public class Solution {
@@ -14,7 +17,36 @@ public class Solution {
 
     public static boolean isNormalLockOrder(final Solution solution, final Object o1, final Object o2) throws Exception {
         //do something here
-        return false;
+        Thread t1 = null;
+        Thread t2 = null;
+        for(int i = 0; i < 20; i++){
+            t1 = new Thread(){
+                @Override
+                public void run() {
+                    for (int j = 0; j < 20; j++) {
+                        solution.someMethodWithSynchronizedBlocks(o1, o2);
+                    }
+                }
+            };
+            t1.start();
+
+            t2 = new Thread(){
+                @Override
+                public void run() {
+                    for (int j = 0; j < 20; j++) {
+                        synchronized (o1) {
+                            synchronized (o2) {
+                                System.out.println("my_thread_" + o1 + " " + o2);
+                            }
+                        }
+                    }
+                }
+            };
+            t2.start();
+        }
+        sleep(1000);
+        return !(t1.getState() == Thread.State.BLOCKED && t2.getState() == Thread.State.BLOCKED);
+        //return false;
     }
 
     public static void main(String[] args) throws Exception {
