2d8e57d83fba157d709d5a20bd82661acfebd93e
knastnt <456kot@mail.ru>
2018-11-06 01:15:46 +1000

    вреде работает, но невалидно

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,6 +1,10 @@
 package com.javarush.task.task31.task3110;
 
+import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
     private Path zipFile;
@@ -10,6 +14,20 @@ public class ZipFileManager {
     }
 
     public void createZip(Path source) throws Exception{
+        //try(ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(zipFile.toFile()))) {
+        try(ZipOutputStream zipOutputStream = (ZipOutputStream) Files.newOutputStream(zipFile)) {
+            ZipEntry zipEntry = new ZipEntry(source.getFileName().toString());
+            zipOutputStream.putNextEntry(zipEntry);
 
+            //try (InputStream inputStream = new FileInputStream(source.toFile())) {
+            try (InputStream inputStream = Files.newInputStream(source)) {
+                byte[] buffer = new byte[1024];
+                while(inputStream.available()>0){
+                    int count = inputStream.read(buffer);
+                    zipOutputStream.write(buffer,0,count);
+                }
+            }
+            zipOutputStream.closeEntry();
+        }
     }
 }
