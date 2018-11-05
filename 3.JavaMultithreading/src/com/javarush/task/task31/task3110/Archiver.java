477323400f3dfa7391fcc072710f4ff4270879ac
knastnt <456kot@mail.ru>
2018-11-06 00:14:20 +1000

    3109

---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task31.task3110;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Archiver {
+    public static void main(String[] args) {
+        System.out.println("Введите полный путь к будущему архиву:");
+        BufferedReader brdr = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = null;
+        try {
+            fileName = brdr.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(fileName));
+
+        System.out.println("Введите полный путь к файлу:");
+        try {
+            fileName = brdr.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            zipFileManager.createZip(Paths.get(fileName));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
