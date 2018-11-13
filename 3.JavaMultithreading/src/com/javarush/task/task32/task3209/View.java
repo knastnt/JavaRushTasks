37a98d89252cc7d9b43c8a8d4d8fd37c495455bf
knastnt <456kot@mail.ru>
2018-11-13 22:39:16 +1000

    html4 in progress

---
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -0,0 +1,52 @@
+package com.javarush.task.task32.task3209;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class View extends JFrame implements ActionListener {
+    private Controller controller;
+
+    private JTabbedPane tabbedPane = new JTabbedPane(); //это будет панель с двумя вкладками.
+    private JTextPane htmlTextPane = new JTextPane(); //это будет компонент для визуального редактирования html.
+    private JEditorPane plainTextPane = new JEditorPane(); //это будет компонент для редактирования html в виде текста, он будет отображать код html (теги и их содержимое).
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public Controller getController() {
+        return controller;
+    }
+
+    public void init(){
+        initGui();
+        addWindowListener(new);
+    }
+
+    public void initGui(){
+        initMenuBar();
+        initEditor();
+        pack();
+    }
+
+    //инициализация меню
+    public void initMenuBar(){
+
+    }
+
+    //инициализация панелей редактора
+    public void initEditor(){
+
+    }
+
+    public void exit(){
+        controller.exit();
+    }
+
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+}
