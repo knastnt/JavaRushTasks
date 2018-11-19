60b17f00451dca63c44455c446ab11bd21319f1f
knastnt <456kot@mail.ru>
2018-11-20 09:33:40 +1000

    траблы с мерджем

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -19,7 +19,7 @@ public class Controller {
     }
 
     public void init(){
-
+        createNewDocument();
     }
 
     public void exit(){
@@ -85,7 +85,11 @@ public class Controller {
     }
 
     public void createNewDocument(){
-
+        view.selectHtmlTab();
+        resetDocument();
+        view.setTitle("HTML редактор");
+        view.resetUndo();
+        currentFile = null;
     }
     public void openDocument(){
 
