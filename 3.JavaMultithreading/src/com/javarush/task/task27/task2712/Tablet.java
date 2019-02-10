6f890a034914c1e2053350e2dc8e3a40c68140cc
knastnt <456kot@mail.ru>
2019-02-10 15:30:27 +1000

    2712 - 8

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Tablet.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Tablet.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task27.task2712;
 
+import com.javarush.task.task27.task2712.ad.AdvertisementManager;
 import com.javarush.task.task27.task2712.kitchen.Order;
 
 import java.io.IOException;
@@ -22,6 +23,12 @@ public class Tablet extends Observable {
             ConsoleHelper.writeMessage(order.toString());
             setChanged();
             notifyObservers(order);
+            //new Thread(){
+            //    @Override
+            //    public void run() {
+                    new AdvertisementManager(order.getTotalCookingTime() * 60).processVideos();
+            //    }
+            //}.start();
             return order;
         } catch (IOException e) {
             logger.log(Level.SEVERE, "Console is unavailable.");
--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/Advertisement.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/Advertisement.java
@@ -7,11 +7,28 @@ public class Advertisement {
     private int hits; // - количество оплаченных показов
     private int duration; // - продолжительность в секундах
 
+    private long amountPerOneDisplaying;
+
     public Advertisement(Object content, String name, long initialAmount, int hits, int duration) {
         this.content = content;
         this.name = name;
         this.initialAmount = initialAmount;
         this.hits = hits;
         this.duration = duration;
+
+        amountPerOneDisplaying = initialAmount / hits;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public long getAmountPerOneDisplaying() {
+        return amountPerOneDisplaying;
     }
 }
