9f79e61dfef278979e9bece64f98cdaed051a5d3
knastnt <456kot@mail.ru>
2019-02-25 13:05:34 +1000

    2712 - 15

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
@@ -1,16 +1,15 @@
 package com.javarush.task.task27.task2712.statistic;
 
+import com.javarush.task.task27.task2712.kitchen.Cook;
 import com.javarush.task.task27.task2712.statistic.event.EventDataRow;
 import com.javarush.task.task27.task2712.statistic.event.EventType;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class StatisticManager {
     private static StatisticManager statisticManager;
     private StatisticStorage statisticStorage = new StatisticStorage();
+    private Set cooks = new HashSet<Cook>();
 
     private StatisticManager() {
     }
@@ -22,6 +21,10 @@ public class StatisticManager {
 
     public void register(EventDataRow data){
         statisticStorage.put(data);
+    }
+
+    public void register(Cook cook) {
+        cooks.add(cook);
     }
 
     private class StatisticStorage {
