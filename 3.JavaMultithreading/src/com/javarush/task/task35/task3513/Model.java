8fdd2e1914e09b70b4a9c16698d2fe50724f02ca
knastnt <456kot@mail.ru>
2019-03-02 15:23:53 +1000

    3513 - 3

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -1,4 +1,14 @@
 package com.javarush.task.task35.task3513;
 
 public class Model {
+    private static final int FIELD_WIDTH = 4;
+    private Tile[][] gameTiles = new Tile[4][4];
+
+    public Model() {
+        for (int i = 0; i < FIELD_WIDTH; i++) {
+            for (int j = 0; j < FIELD_WIDTH; j++) {
+                gameTiles[i][j] = new Tile();
+            }
+        }
+    }
 }
