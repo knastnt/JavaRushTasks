eb00fec97324e29bed988a74af51a6577b38978f
knastnt <456kot@mail.ru>
2019-03-13 08:26:57 +1000

    try with resources

--- 4.JavaCollections/src/com/javarush/task/task31/task3101/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3101/Solution.java
@@ -9,36 +9,32 @@ import java.util.Comparator;
 Проход по дереву файлов
 */
 public class Solution {
-    private static ArrayList<File> list = new ArrayList<>();
+    private ArrayList<File> list = new ArrayList<>();
 
     public static void main(String[] args) {
+        Solution me = new Solution();
         String path = args.length>0 ? args[0] : "D:\\";
         String resultFileAbsolutePath = args.length>1 ? args[1] : "D:\\result.txt";
         File resultFile = new File(resultFileAbsolutePath);
 
-        printFiles(new File(path));
+        me.printFiles(new File(path));
 
-        Collections.sort(list, new Comparator<File>() {
+        Collections.sort(me.list, new Comparator<File>() {
             @Override
             public int compare(File o1, File o2) {
                 return o1.getName().toLowerCase().compareTo(o2.getName().toLowerCase());
             }
         });
 
-        /*for (File file : list) {
-            System.out.println(file.getName());
-        }*/
-
         if (FileUtils.isExist(resultFile)){
             //System.out.println("success");
             File renamed = new File(resultFile.getParent() + "/allFilesContent.txt");
             FileUtils.renameFile(resultFile, renamed);
 
-            try {
-                OutputStreamWriter osw = new FileWriter(renamed);
-                for (File file : list) {
-                    try {
-                        InputStreamReader isr = new FileReader(file);
+
+            try ( OutputStreamWriter osw = new FileWriter(renamed) ) {
+                for (File file : me.list) {
+                    try ( InputStreamReader isr = new FileReader(file) ) {
                         while (isr.ready()) {
                             osw.write(isr.read());
                         }
@@ -48,7 +44,6 @@ public class Solution {
                         System.out.println("I/O Error on file " + file.getAbsolutePath());
                     }
                 }
-                osw.close();
                 System.out.println("success");
             }catch (IOException e){
                 System.out.println("I/O Error");
@@ -58,7 +53,7 @@ public class Solution {
         }
     }
 
-    private static void printFiles(File f){
+    private void printFiles(File f){
         if (f.listFiles() == null) return;
         for (File file : f.listFiles())
         {
