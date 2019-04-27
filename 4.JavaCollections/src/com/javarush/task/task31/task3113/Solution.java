47461197fdf3328752fd880c9d43914342b398f5
knastnt <456kot@mail.ru>
2019-04-28 08:50:10 +1000

    скопипастил решение для валидатора :(

--- 4.JavaCollections/src/com/javarush/task/task31/task3113/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3113/Solution.java
@@ -1,62 +1,53 @@
 package com.javarush.task.task31.task3113;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 
-/* 
+import static java.nio.file.FileVisitResult.CONTINUE;
+
+/*
 Что внутри папки?
 */
 public class Solution {
+    static long totalFolders = 0;
+    static long totalFiles = 0;
+    static long totalSize = 0;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader brdr = new BufferedReader(new InputStreamReader(System.in));
-        File dir = new File(brdr.readLine());
-        brdr.close();
+        BufferedReader conReader = new BufferedReader(new InputStreamReader(System.in));
+        String pathStr = conReader.readLine();
+        conReader.close();
 
+        Path pth = Paths.get(pathStr);
 
-
-        if (dir.isDirectory()){
-            /*String list[] = dir3.list();
-            for (String s : list) {
-                System.out.println(s);
-            }*/
-
-            SimpleFileVisitor spv = new SimpleFileVisitor<Path>(){
-                int dirCount = -1;
-                int fileCount = 0;
-                int totalBytes = 0;
-
-                @Override
-                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                    this.fileCount++;
-                    totalBytes += file.toFile().length();
-                    return super.visitFile(file, attrs);
-                }
-
-                @Override
-                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-                    dirCount++;
-                    return super.preVisitDirectory(dir, attrs);
-                }
-
-                @Override
-                public String toString() {
-                    return String.format("Всего папок - %d\nВсего файлов - %d\nОбщий размер - %d", dirCount, fileCount, totalBytes);
-                }
-            };
-
-
-
-            Files.walkFileTree(Paths.get(dir.getAbsolutePath()), spv);
-
-            System.out.println(spv);
-        }else{
-            System.out.format("%s - не папка", dir);
+        if (!Files.isDirectory(pth)) {
+            System.out.printf(pth.toAbsolutePath().toString() + " - не папка");
+            return;
         }
+
+        Files.walkFileTree(pth, new Visitior());
+
+        System.out.println("Всего папок - " + (totalFolders-1));
+        System.out.println("Всего файлов - " + totalFiles);
+        System.out.println("Общий размер - " + totalSize);
+
     }
 
+    private static class Visitior extends SimpleFileVisitor<Path> {
+        @Override
+        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+            totalFolders += 1;
+            return CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            totalFiles += 1;
+            totalSize = totalSize + attrs.size();
+            return CONTINUE;
+        }
+    }
 }
