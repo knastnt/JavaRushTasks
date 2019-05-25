cddeecd6f131591cf26d8620f95d67421ebc98c4
knastnt <456kot@mail.ru>
2019-05-26 08:05:43 +1000

    3202

---
+++ 4.JavaCollections/src/com/javarush/task/task32/task3202/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task32.task3202;
+
+import java.io.*;
+
+/* 
+Читаем из потока
+*/
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        StringWriter writer = getAllDataFromInputStream(new FileInputStream("testFile.log"));
+        System.out.println(writer.toString());
+    }
+
+    public static StringWriter getAllDataFromInputStream(InputStream is) throws IOException {
+        StringWriter sw = new StringWriter();
+        try {
+            while (is.available() > 0) {
+                sw.write(is.read());
+            }
+        } catch (NullPointerException e) {
+            return sw;
+        }
+        return sw;
+    }
+}
