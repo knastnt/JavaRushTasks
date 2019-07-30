71c321389300171fc3add3e2b756a2f65b714287
knastnt <456kot@mail.ru>
2019-07-30 14:31:36 +1000

    2213 - 12

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
@@ -17,6 +17,19 @@ public class Field {
         return matrix;
     }
 
+    public void print() { //объект будет отрисовывать на экран свое текущее состояние;
+
+    }
+    public void removeFullLines() { //будет удалять из матрицы полностью заполненные строки и сдвигать вышележащие строки вниз;
+
+    }
+    public Integer getValue(int x, int y) { //возвращает значение которое находится в матрице с координатами x и y;
+        return null;
+    }
+    public void setValue(int x, int y, int value) { //устанавливает переданное значение в ячейку массива (матрицы) с координатами x, y.
+
+    }
+
     public Field(int width, int height) {
         this.width = width;
         this.height = height;
--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
@@ -1,4 +1,51 @@
 package com.javarush.task.task22.task2213;
 
 public class Figure {
+    private int x;
+    private int y;
+    private int[][] matrix;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    public void left() { //для движения фигурки влево.
+
+    }
+    public void right() { //для движения фигурки вправо.
+
+    }
+    public void down() { //для движения фигурки вниз.
+
+    }
+    public void up() { //для движения фигурки вверх.
+
+    }
+    public void rotate() { //для поворота фигурки вокруг главной диагонали.
+
+    }
+    public void downMaximum() { //падение фигурки в низ до дна.
+
+    }
+    public boolean isCurrentPositionAvailable() { //проверка - может ли фигурка быть помещена в текущую позицию. Для теста захардкодь результат в true, пока мы не реализовали логику.
+        return true;
+    }
+    public void landed() { //вызывается, когда фигурка достигла дна или уперлась в другую фигурку. Все ее занятые клетки теперь должны добавиться в Field.
+
+    }
+
+
+    public Figure(int x, int y, int[][] matrix) {
+        this.x = x;
+        this.y = y;
+        this.matrix = matrix;
+    }
 }
