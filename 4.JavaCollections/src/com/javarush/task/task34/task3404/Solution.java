360d153282f1e74463d0edae51245a17f6e9efd4
knastnt <456kot@mail.ru>
2019-10-12 22:28:44 +1000

    пытается считать

--- 4.JavaCollections/src/com/javarush/task/task34/task3404/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3404/Solution.java
@@ -9,25 +9,47 @@ import java.util.regex.Pattern;
 public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        solution.recurse("(1.5*4.2)", 0); //expected output 0.5 6
+        System.out.println(solution.recurse("5*sin(1.7754*5.2)+28", 0)); //expected output 0.5 6
 //        solution.recurse("sin(2*(-5+1.5*4)+28)", 0); //expected output 0.5 6
     }
 
-    public double recurse(final String expression, int countOperation) {
+    public String recurse(final String expression, int countOperation) {
         String cleanExpression = expression.replace(" ", "");
 //        Pattern p = Pattern.compile("(.+)\\.part(\\d+)$");
 //        Matcher m = p.matcher(testString);
 //        //return m.matches();
 //        m.find();
 //        return m.group(1);
-        Pattern p = Pattern.compile("(^.*)([0-9]+(\\.[0-9]+)?)(\\*|\\^)([0-9]+(\\.[0-9]+)?)(.*$)");
+//        Pattern p = Pattern.compile("(^.*)([0-9]+(\\.[0-9]+)?)(\\*|\\^)([0-9]+(\\.[0-9]+)?)(.*$)");
+//        Pattern p = Pattern.compile("(^|[^0-9])([0-9]+(\\.[0-9]+)?)($|[^0-9])"); //double
+//        Pattern p = Pattern.compile(  "(^|^.*[^0-9\\.])([0-9]+(\\.[0-9]+)?)(\\*|\\^)([0-9]+(\\.[0-9]+)?)($|[^0-9\\.].*$)"  ); //double
+        Pattern p = Pattern.compile(  "(^|^.*[^0-9\\.])([0-9]+(\\.[0-9]+)?)(\\*|\\^)([0-9]+(\\.[0-9]+)?)($|[^0-9\\.].*$)"  ); //double
         Matcher m = p.matcher(cleanExpression);
         //return m.matches();
         m.find();
-        for (int i=0; i<m.groupCount(); i++){
-            System.out.println(m.group(i));
+        for (int i=0; i<=m.groupCount(); i++){
+//            System.out.println(m.group(i));
         }
-        return 0;
+        String start = m.group(1);
+        String end = m.group(7);
+        double first = Double.parseDouble(m.group(2));
+        double second = Double.parseDouble(m.group(5));
+        double result = 0;
+        String charact = m.group(4);
+
+        switch (charact){
+            case "*":
+                result = first * second;
+                break;
+        }
+
+        String resultS;
+        if(result<0){
+            resultS = "0-" + String.valueOf(result);
+        }else {
+            resultS = String.valueOf(result);
+        }
+        return start + resultS + end;
         //implement
     }
 
