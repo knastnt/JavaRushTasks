de4e6d818b152c2d67d1370b288fb370950a022a
knastnt <456kot@mail.ru>
2019-10-27 16:12:42 +1000

    3406 какое-то дурацкое скопипащенное решение метода insert. и в итоге х.з. какая-то хрень получается в дереве

---
+++ 4.JavaCollections/src/com/javarush/task/task36/task3604/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task36.task3604;                                                  
+                                                  
+/*                                                   
+Разбираемся в красно-черном дереве                                                  
+*/                                                  
+public class Solution {                                                  
+    public static void main(String[] args) {
+        RedBlackTree redBlackTree = new RedBlackTree();
+        redBlackTree.insert(12);
+        redBlackTree.insert(7);
+        redBlackTree.insert(25);
+        redBlackTree.insert(3);
+        redBlackTree.insert(10);
+        redBlackTree.insert(20);
+        redBlackTree.insert(100);
+        redBlackTree.insert(65);
+        redBlackTree.insert(250);
+        redBlackTree.insert(500);
+        System.out.println("");
+    }
+}
