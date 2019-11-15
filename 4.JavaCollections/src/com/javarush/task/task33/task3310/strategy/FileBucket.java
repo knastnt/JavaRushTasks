e44b6540f35c896947fcb995698bdf92aa2359c9
knastnt <456kot@mail.ru>
2019-11-15 15:19:15 +1000

    3310 - 10 осталось 2 пункта валидатора

--- 4.JavaCollections/src/com/javarush/task/task33/task3310/strategy/FileBucket.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/strategy/FileBucket.java
@@ -1,64 +1,44 @@
 package com.javarush.task.task33.task3310.strategy;
 
-import javax.tools.FileObject;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.attribute.FileAttribute;
 
 public class FileBucket {
     private Path path;
 
     public FileBucket() {
         try {
-            path = Files.createTempFile("",null);
-            File file = path.toFile();
-            file.deleteOnExit();
+            path = Files.createTempFile(null,null);
+
+            Files.deleteIfExists(path);
+            Files.createFile(path);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+        path.toFile().deleteOnExit();
     }
 
     public long getFileSize() { //, он должен возвращать размер файла на который указывает path.
-        return path.toFile().length();
+        try {
+            return Files.size(path);
+        } catch (IOException e) {
+        }
+        return -1;
     }
     public void putEntry(Entry entry) { //- должен сериализовывать переданный entry в файл. Учти, каждый entry может содержать еще один entry.
         try {
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-
-            ObjectOutputStream oos = new ObjectOutputStream(baos);
-            oos.writeObject(entry);
-            oos.close();
-
-            baos.flush();
-
-            FileOutputStream fos = new FileOutputStream(path.toFile());
-            fos.write(baos.toByteArray());
-            fos.close();
-
-            baos.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(path));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
     public Entry getEntry() { //- должен забирать entry из файла. Если файл имеет нулевой размер, вернуть null.
-        if (path.toFile().length() == 0) return null;
+        if (this.getFileSize() <= 0) return null;
         try {
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-
-            BufferedInputStream bis = new BufferedInputStream(new FileInputStream(path.toFile()));
-            baos.write(bis.readAllBytes());
-            bis.close();
-            baos.flush();
-
-            ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(baos.toByteArray()));
-            Entry toReturn = (Entry) ois.readObject();
-            ois.close();
-            baos.close();
-
-            return toReturn;
+            ObjectInputStream ois = new ObjectInputStream(Files.newInputStream(path));
+            return (Entry) ois.readObject();
         } catch (IOException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
@@ -67,6 +47,10 @@ public class FileBucket {
         return null;
     }
     public void remove() { //- удалять файл на который указывает path.
-        path.toFile().delete();
+        try {
+            Files.deleteIfExists(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
