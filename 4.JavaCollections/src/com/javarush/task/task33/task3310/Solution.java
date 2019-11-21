2dce9d7d0a37e0480c4dfca589e88a21fb93b6d3
knastnt <456kot@mail.ru>
2019-11-21 22:40:44 +1000

    3310 - 12

--- 4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
@@ -10,7 +10,7 @@ public class Solution {
     public static void main(String[] args) {
 //        testStrategy(new HashMapStorageStrategy(), 10000);
 //        testStrategy(new OurHashMapStorageStrategy(), 10000);
-        testStrategy(new FileStorageStrategy(), 100);
+        testStrategy(new OurHashBiMapStorageStrategy(), 10000);
 
 //        FileBucket fb = new FileBucket();
 //        fb.putEntry(new Entry(555, 7777L, "Привет!", new Entry(444, 6666L, "Пока...", null)));
