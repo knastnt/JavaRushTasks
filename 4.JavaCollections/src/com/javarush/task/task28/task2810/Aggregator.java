4c72379f78c3f9f0e51d678ed8e49fc007a76bd7
knastnt <456kot@mail.ru>
2019-12-04 23:34:43 +1000

    2810 - 2

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
@@ -1,7 +1,9 @@
 package com.javarush.task.task28.task2810;
 
+import com.javarush.task.task28.task2810.model.Provider;
+
 public class Aggregator {
     public static void main(String[] args) {
-
+        System.out.println(new Controller(new Provider(null)));
     }
 }
