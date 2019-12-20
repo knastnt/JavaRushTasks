6e8131a31f506c5beaeacc479ccf2e89516d8338
knastnt <456kot@mail.ru>
2019-12-20 19:53:51 +1000

    2810-11

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
@@ -5,11 +5,5 @@ import com.javarush.task.task28.task2810.model.Provider;
 
 public class Aggregator {
     public static void main(String[] args) {
-        try {
-            Controller controller = new Controller(new Provider(new HHStrategy()));
-            controller.scan();
-        }catch (NullPointerException e){
-
-        }
     }
 }
