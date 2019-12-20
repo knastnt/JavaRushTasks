7c0e1454b86dfddff26d02a0eed9e9759a01d8df
knastnt <456kot@mail.ru>
2019-12-20 20:01:53 +1000

    2810-12

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
@@ -1,9 +1,17 @@
 package com.javarush.task.task28.task2810;
 
 import com.javarush.task.task28.task2810.model.HHStrategy;
+import com.javarush.task.task28.task2810.model.Model;
 import com.javarush.task.task28.task2810.model.Provider;
+import com.javarush.task.task28.task2810.view.HtmlView;
+import com.javarush.task.task28.task2810.view.View;
 
 public class Aggregator {
     public static void main(String[] args) {
+        View view = new HtmlView();
+        Model model = new Model(view, new Provider(new HHStrategy()));
+        Controller controller = new Controller(model);
+        view.setController(controller);
+        ((HtmlView)view).userCitySelectEmulationMethod();
     }
 }
