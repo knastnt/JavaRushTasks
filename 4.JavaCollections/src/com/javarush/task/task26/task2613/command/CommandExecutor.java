7d5f650b1ee7ea2cd60ba4d720d748510dc02950
knastnt <456kot@mail.ru>
2019-12-28 21:14:44 +1000

    2613 - 7

---
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/command/CommandExecutor.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task26.task2613.command;
+
+import com.javarush.task.task26.task2613.Operation;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CommandExecutor {
+    private static final Map<Operation, Command> allKnownCommandsMap = new HashMap<>();
+    static {
+        allKnownCommandsMap.put(Operation.INFO, new InfoCommand());
+        allKnownCommandsMap.put(Operation.DEPOSIT, new DepositCommand());
+        allKnownCommandsMap.put(Operation.WITHDRAW, new WithdrawCommand());
+        allKnownCommandsMap.put(Operation.EXIT, new ExitCommand());
+    }
+    public static final void execute(Operation operation){
+        if (!allKnownCommandsMap.containsKey(operation)) throw new IllegalArgumentException();
+        allKnownCommandsMap.get(operation).execute();
+    }
+
+    private CommandExecutor() {
+    }
+}
