94f6e60ee6770044de075a58dd3d5a5b7e3c11b4
knastnt <456kot@mail.ru>
2020-01-03 22:31:58 +1000

    3410 - 3

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/controller/Controller.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/controller/Controller.java
@@ -10,9 +10,10 @@ public class Controller {
     public Controller() {
         this.view = new View(this);
         this.model = new Model();
+        this.view.init();
     }
 
     public static void main(String[] args) {
-
+        new Controller();
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task34/task3410/view/View.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/view/View.java
@@ -1,14 +1,25 @@
 package com.javarush.task.task34.task3410.view;
 
-import javax.swing.*;
 import com.javarush.task.task34.task3410.controller.Controller;
 
-import java.awt.*;
+import javax.swing.*;
 
 public class View extends JFrame {
     private Controller controller;
+    private Field field;
 
-    public View(Controller controller) throws HeadlessException {
+    public View(Controller controller) {
         this.controller = controller;
+    }
+
+    public void init() {
+        field = new Field(this);
+        add(field);
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+        setTitle("Сокобан");
+        setVisible(true);
     }
 }
